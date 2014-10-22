735cb12c0202e3aa62579018c89bd92462dbeebd
Rysiw <bionic@bionics-iMac.local>
2014-10-22 20:43:38 +0300

    Third Lesson Commit

---
+++ src/test/java/Simple/FirstTest.java
@@ -0,0 +1,19 @@
+package Simple;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+/**
+ * Created by bionic on 10/22/14.
+ */
+public class FirstTest {
+
+    @Test
+    public void firstTest(){
+        System.out.println("first");
+        Assert.assertTrue(true);
+    }
+
+
+
+}
